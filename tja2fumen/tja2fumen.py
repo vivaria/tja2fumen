4995a4bee169f493974afd0b31ae846f81bd92dc
Viv <ms.vivaria@gmail.com>
2023-06-02 16:33:42 -0400

    `tja2fumen.py`: Differentiate between note types
    Having multiple note values map to identical strings
    (e.g. "Don", "Ka", "DON", "KA") caused issues when
    writing these strings back to .fumen files.
    
    So, we make sure that each note type has a unique
    string associated with it.

--- tja2fumen/tja2fumen.py
+++ tja2fumen/tja2fumen.py
@@ -8,33 +8,33 @@ tja2fumen_version = "v0.1"
 branchNames = ("normal", "advanced", "master")
 
 noteTypes = {
-    0x1: "Don",  # ドン
-    0x2: "Don",  # ド
-    0x3: "Don",  # コ
-    0x4: "Ka",   # カッ
-    0x5: "Ka",   # カ
+    0x1: "Don",   # ドン
+    0x2: "Don2",  # ド
+    0x3: "Don3",  # コ
+    0x4: "Ka",    # カッ
+    0x5: "Ka2",   # カ
     0x6: "Drumroll",
     0x7: "DON",
     0x8: "KA",
     0x9: "DRUMROLL",
     0xa: "Balloon",
-    0xb: "DON",       # hands
+    0xb: "DON2",        # hands
     0xc: "Kusudama",
-    0xd: "KA",        # hands
-    0xe: "?",         # ? (Present in some Wii1 songs)
-    0xf: "?",         # ? (Present in some PS4 songs)
-    0x10: "?",        # ? (Present in some Wii1 songs)
-    0x11: "?",        # ? (Present in some Wii1 songs)
-    0x12: "?",        # ? (Present in some Wii4 songs)
-    0x13: "?",        # ? (Present in some Wii1 songs)
-    0x14: "?",        # ? (Present in some PS4 songs)
-    0x15: "?",        # ? (Present in some Wii1 songs)
-    0x16: "?",        # ? (Present in some Wii1 songs)
-    0x17: "?",        # ? (Present in some Wii4 songs)
-    0x18: "?",        # ? (Present in some PS4 songs)
-    0x19: "?",        # ? (Present in some PS4 songs)
-    0x22: "?",        # ? (Present in some Wii1 songs)
-    0x62: "Drumroll"  # ?
+    0xd: "KA2",         # hands
+    0xe: "Unknown1",    # ? (Present in some Wii1 songs)
+    0xf: "Unknown2",    # ? (Present in some PS4 songs)
+    0x10: "Unknown3",   # ? (Present in some Wii1 songs)
+    0x11: "Unknown4",   # ? (Present in some Wii1 songs)
+    0x12: "Unknown5",   # ? (Present in some Wii4 songs)
+    0x13: "Unknown6",   # ? (Present in some Wii1 songs)
+    0x14: "Unknown7",   # ? (Present in some PS4 songs)
+    0x15: "Unknown8",   # ? (Present in some Wii1 songs)
+    0x16: "Unknown9",   # ? (Present in some Wii1 songs)
+    0x17: "Unknown10",  # ? (Present in some Wii4 songs)
+    0x18: "Unknown11",  # ? (Present in some PS4 songs)
+    0x19: "Unknown12",  # ? (Present in some PS4 songs)
+    0x22: "Unknown13",  # ? (Present in some Wii1 songs)
+    0x62: "Drumroll2"   # ?
 }
 
 # Fumen headers are made up of smaller substrings of bytes
