08c0e52bc0b4ad5728336f7fa5bdc2f2c38c7c19
Viv <ms.vivaria@gmail.com>
2023-07-01 11:30:56 -0400

    Add `hol6po` for testing branch support

--- testing/test_conversion.py
+++ testing/test_conversion.py
@@ -12,6 +12,7 @@ from tja2fumen.constants import COURSE_IDS, NORMALIZE_COURSE, simpleHeaders, byt
 
 
 @pytest.mark.parametrize('id_song', [
+    pytest.param('hol6po'),
     pytest.param('mikdp'),
     pytest.param('ia6cho'),
 ])
