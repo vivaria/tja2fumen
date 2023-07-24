31366a014342360b5dc67e9965c181b6ab408cac
Viv <ms.vivaria@gmail.com>
2023-07-24 03:14:24 +0000

    Address Pylint warnings

--- testing/conftest.py
+++ testing/conftest.py
@@ -18,10 +18,10 @@ def entry_point(request):
     return request.config.getoption("--entry-point")
 
 
-def convert(path_test, path_tja_tmp, entry_point, err_msg=None):
+def convert(path_test, path_tja_tmp, entry_point_arg, err_msg=None):
     tb = ''
 
-    if entry_point == "python-api":
+    if entry_point_arg == "python-api":
         if not err_msg:
             api_convert(argv=[path_tja_tmp])
         else:
@@ -29,7 +29,7 @@ def convert(path_test, path_tja_tmp, entry_point, err_msg=None):
                 api_convert(argv=[path_tja_tmp])
             tb = "".join(traceback.format_tb(e.tb))
 
-    elif entry_point == "python-cli":
+    elif entry_point_arg == "python-cli":
         if not err_msg:
             subprocess.check_output(f"tja2fumen {path_tja_tmp}", text=True,
                                     shell=True, stderr=subprocess.STDOUT)
@@ -39,7 +39,7 @@ def convert(path_test, path_tja_tmp, entry_point, err_msg=None):
                                         shell=True, stderr=subprocess.STDOUT)
             tb = e.value.output
 
-    elif entry_point == "exe":
+    elif entry_point_arg == "exe":
         exe_glob = os.path.join(os.path.split(path_test)[0], "dist", "*.exe")
         exe = glob.glob(exe_glob)[0]
         if not err_msg:
